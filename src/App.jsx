401f7e84b4bc6f374220a7c9087819e425e23b51
PengWin553 <158315168+PengWin553@users.noreply.github.com>
2024-07-13 22:56:07 +0800

    Added Title and minimal UI adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,25 @@ function App() {
   return (
     <>
       <div className="score-board">
+        <h1 className='title'>Score Board</h1>
+        
+        <div className="container">
+          <div className="team">
+            <h1 className="team-name">Team A</h1>
+            <Card />
+          </div>
 
-        <div className="team">
-          <h1 className="team-name">Team A</h1>
-          <Card />
-        </div>
+          <div className="round">
+            <h1 className="team-name">Round</h1>
+            <Card />
+          </div>
 
-        <div className="round">
-          <h1 className="team-name">Round</h1>
-          <Card />
+          <div className="team">
+            <h1 className="team-name">Team B</h1>
+            <Card />
+          </div>
         </div>
-
-        <div className="team">
-          <h1 className="team-name">Team B</h1>
-          <Card />
-        </div>
+      
 
       </div>
     </>
