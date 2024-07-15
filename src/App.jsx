dcd7d516fd2c4444ee7024bbe8b6749ed2c79fdc
PengWin553 <158315168+PengWin553@users.noreply.github.com>
2024-07-15 17:55:24 +0800

    Finished it

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+// props = read-only properties that are shared between componenets.
+//         A parent componenet can send data to a child component.
+//         <Component key=value />
+
 import Student from './Student.jsx'
 import './index.css'
 
@@ -8,6 +12,12 @@ function App() {
       <Student name="Peng Win" age={21} isStudent={true} />
       <Student name="Akuma" age={26} isStudent={false} />
       <Student name="Kobayashi" age={25} isStudent={false} />
+
+      {/* this has all the defaultProps values */}
+      <Student />
+
+      {/* this has only two of the defaultProps values, since it is already set a name  */}
+      <Student name="Cat"/>
     </>
   )
 }
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,3 +1,13 @@
+// propTypes = a mechanism that ensures that the passed value is of the correct datatype.
+//              ex: propTypes.number
+// it won't stop the application from running; it is good for debugging
+// it is good practice to include proptypes if you're using props
+
+// defaultProps = default values for props in case they are not passed from the parent componenet
+//              ex: name: "DefaultGuest"
+
+import PropTypes from 'prop-types'
+
 function Student(props){
 
     return(
@@ -8,6 +18,20 @@ function Student(props){
         </div>
     )
    
+}
+
+// props
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool,
+}
+
+// default props
+Student.defaultProps = {
+    name: "Unknown",
+    age: 0,
+    isStudent: false,
 }
 
 export default Student
