4baf00e1a33fbd3273a2c30b2e900fb734feaa02
PengWin553 <158315168+PengWin553@users.noreply.github.com>
2024-07-15 09:52:53 +0800

    Props

---
+++ src/Student.jsx
@@ -0,0 +1,13 @@
+function Student(props){
+
+    return(
+        <div className="student-container">
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>Student: {props.isStudent ? "Yes" : "No"}</p>
+        </div>
+    )
+   
+}
+
+export default Student
