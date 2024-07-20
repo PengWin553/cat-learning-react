47960f5d97a8a6a82a067488bb350e96219e8e36
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-07-20 19:40:12 +0800

    Created Simple Calcu with UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+  const [result, setResult] = useState(0);
+  const [operator, setOperator] = useState('');
+
+  const handleChangeA = (e) => {
+    setA(Number(e.target.value));
+  };
+
+  const handleChangeB = (e) => {
+    setB(Number(e.target.value));
+  };
+
+  // Math Functions
+  function Addition(){
+    setResult(a + b);
+  }
+
+  function Subtraction(){
+    setResult(a - b);
+  }
+
+  function Multiplication(){
+    setResult(a * b);
+  }
+
+  function Division(){
+    setResult(a / b);
+  }
+
+  function Solve(){
+    operator === '+' ? Addition() :
+    operator === '-' ? Subtraction() :
+    operator === '*' ? Multiplication() :
+    operator === '/' ? Division() : ""
+  }
+
+  useEffect(() => {
+    Solve();
+  },[a,b,result])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="calculator-container">
+
+        <h3>Simple Calculator</h3>
+
+        {/* first value */}
+        <div className="input-container">
+          <label htmlFor="a">A:</label>
+          <input type="text" id="a" value={a} onChange={handleChangeA} placeholder="Enter first value" />
+        </div>
+
+        <br></br>
+
+        {/* second value */}
+        <div className="input-container">
+          <label htmlFor="b">B:</label>
+          <input type="text" id="b" value={b} onChange={handleChangeB} placeholder="Enter second value" />
+        </div>
+
+        <br></br>
+
+        {/* output container */}
+        <div className="output">
+          <label htmlFor="b">Output:</label>
+          <input type="text" id="output" value={result} readOnly placeholder="Output" />
+        </div>
+
+        <br></br>
+
+        {/* buttons */}
+        <div className="buttons-container">
+          <button className="btn plus" onClick={() => {setOperator('+'), Addition()} }>+</button>
+          <button className="btn minus" onClick={() => {setOperator('-'), Subtraction()}}>-</button>
+          <button className="btn times" onClick={() => {setOperator('*'), Multiplication()}}>*</button>
+          <button className="btn divide" onClick={() => {setOperator('/'), Division()}}>/</button>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
