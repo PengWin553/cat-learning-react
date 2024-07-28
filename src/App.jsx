e51a9dc500448e6032cb7e5c70c8ae22012874ce
PengWin553 <158315168+PengWin553@users.noreply.github.com>
2024-07-29 00:11:08 +0800

    H1 text value alternates from original to new one on button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// the h1 element's text must change to your name when you first click it; if you click it again, it reverts back to its default value
+import { useState } from "react";
+import "./App.css";
+const App = () => {
+  const [text, setText] = useState('sdf');
 
-function App() {
-  const [count, setCount] = useState(0)
+  const ChangeText = () => {
+    setText(text === 'sdf' ? 'PengWin' : 'sdf');
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  return ( 
+      <div className="parent">
+          <h1>{text}</h1>
+          <div className="btn-click" onClick={ChangeText}>
+              Click
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
 }
-
-export default App
+ 
+export default App;
