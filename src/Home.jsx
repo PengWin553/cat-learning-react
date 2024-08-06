d9fb87fd0e0efdca376d18e14fefa20931e92e8a
PengWin553 <158315168+PengWin553@users.noreply.github.com>
2024-08-06 19:02:11 +0800

    Integrate React Bootstrap for Navbar and create initial homepage

---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <div className="home-container">
+            <h2 className="page-title">Welcome to The Homepage</h2>
+        </div>
+    )
+}
+
+export default Home;
